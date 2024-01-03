5ad2c0878adb7512d128eee72d6f78cabc5716f6
Anton-Barash <60425459+Anton-Barash@users.noreply.github.comm>
2024-01-04 00:57:40 +0800

    bandle

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  entry: "./src/app.js",
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        },
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, './build/js/'),
+    filename: 'bundle.js'
+  }
+};
