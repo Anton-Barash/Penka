a25d2f037139d68a676f17ed7956720bbf785aeb
Anton-Barash <60425459+Anton-Barash@users.noreply.github.comm>
2024-01-15 11:54:36 +0800

    root_2

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,15 @@
-
-import './App.css';
-import BinanceData from './modul/BinanceData';
-import './locales/i18n';
-import 'mdb-react-ui-kit/dist/css/mdb.min.css';
-
+import "./App.css";
+import BinanceData from "./modul/BinanceData";
+import "./locales/i18n";
+import "mdb-react-ui-kit/dist/css/mdb.min.css";
+import RootLayout from "./RootLayout";
 
 function App() {
-
-
-
-
   return (
     <div className="App">
       <header className="App-header">
-        
         <BinanceData></BinanceData>
-
-
+        <RootLayout></RootLayout>
       </header>
     </div>
   );
